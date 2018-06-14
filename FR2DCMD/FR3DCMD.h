99259050bc1909306467d237b83b566064c4ba7d
Frankenstein <33659732+salty-Frankenstein@users.noreply.github.com>
2018-06-14 16:28:20 +0800

    3-axes rotation updated

--- FR2DCMD/FR3DCMD.h
+++ FR2DCMD/FR3DCMD.h
@@ -7,14 +7,14 @@
 // FRSL
 #include<matrix.h>	
 
-namespace FR3DCMD{
+namespace FR3DCMD {
 
 	struct vertex {
 		double x, y, z;
 		int colour;
 	};
 
-	void v_init(vertex &v1, double x, double y, double z,int colour) {
+	void v_init(vertex &v1, double x, double y, double z, int colour) {
 		v1.x = x;
 		v1.y = y;
 		v1.z = z;
@@ -22,7 +22,7 @@ namespace FR3DCMD{
 	}
 	class FR3D_CMD {
 	public:
-		FR3D_CMD(FR2DCMD::FR2D_CMD *H, double x = 0, double y = 0, double z=0,double dist=100) {
+		FR3D_CMD(FR2DCMD::FR2D_CMD *H, double x = 0, double y = 0, double z = 0, double dist = 100) {
 			F2DH = H;
 			ox = x;
 			oy = y;
@@ -32,43 +32,24 @@ namespace FR3DCMD{
 		~FR3D_CMD() {}
 
 		void triangle(vertex v1, vertex v2, vertex v3) {
-			FR2DCMD::vertex v2d1=trans_2d(v1), v2d2 = trans_2d(v2), v2d3 = trans_2d(v3);
-			/*
-			FR2DCMD::v_init(v2d1, v1.x, v1.y, v1.colour);
-			FR2DCMD::v_init(v2d2, v2.x, v2.y, v2.colour);
-			FR2DCMD::v_init(v2d3, v3.x, v3.y, v3.colour);
-			*/
-			/*
-			printf("%lf %lf\n", v1.x, v1.y);
-			printf("%lf %lf\n", v2.x, v2.y);
-			printf("%lf %lf\n", v3.x, v3.y);
-			printf("%lf \n%lf %lf\n",d, v2d1.x, v2d1.y);
-			printf("%lf %lf\n", v2d2.x, v2d2.y);
-			printf("%lf %lf\n", v2d3.x, v2d3.y);
-			system("pause");
-			*/
+			FR2DCMD::vertex v2d1 = trans_2d(v1), v2d2 = trans_2d(v2), v2d3 = trans_2d(v3);
 			F2DH->triangle(v2d1, v2d2, v2d3);
 		}
 
 		FR2DCMD::vertex trans_2d(vertex ori) {
-			matrix o(1,4), trans1(4,4),trans2(4,4);
+			matrix o(1, 4), trans1(4, 4), trans2(4, 4);
 			o.elex(0, 0, ori.x);			o.elex(0, 1, ori.y);			o.elex(0, 2, ori.z);			o.elex(0, 3, 1);
 			trans1.elex(0, 0, 1);			trans1.elex(0, 1, 0);			trans1.elex(0, 2, 0);			trans1.elex(0, 3, 0);
 			trans1.elex(1, 0, 0);			trans1.elex(1, 1, 1);			trans1.elex(1, 2, 0);			trans1.elex(1, 3, 0);
-			trans1.elex(2, 0, 0);			trans1.elex(2, 1, 0);			trans1.elex(2, 2, 1);			trans1.elex(2, 3, 1/d);
+			trans1.elex(2, 0, 0);			trans1.elex(2, 1, 0);			trans1.elex(2, 2, 1);			trans1.elex(2, 3, 1 / (d));
 			trans1.elex(3, 0, 0);			trans1.elex(3, 1, 0);			trans1.elex(3, 2, 0);			trans1.elex(3, 3, 0);
-			/*
-			trans2.elex(0, 0, 1);			trans2.elex(0, 1, 0);			trans2.elex(0, 2, 0);			trans2.elex(0, 3, 0);
-			trans2.elex(1, 0, 0);			trans2.elex(1, 1, 1);			trans2.elex(1, 2, 0);			trans2.elex(1, 3, 0);
-			trans2.elex(2, 0, 0);			trans2.elex(2, 1, 0);			trans2.elex(2, 2, 0);			trans2.elex(2, 3, 0);
-			trans2.elex(3, 0, 0);			trans2.elex(3, 1, 0);			trans2.elex(3, 2, 0);			trans2.elex(3, 3, 1);
-			*/
+
 			matrix ans = o * trans1;
 			FR2DCMD::vertex t;
-			//ans = ans * trans2;
-			double dt=ans.ele(0,3);
-			ans.elex(0, 0, ans.ele(0,0)/dt);
-			ans.elex(0, 1, ans.ele(0, 1)/dt);
+
+			double dt = ans.ele(0, 3);
+			ans.elex(0, 0, ans.ele(0, 0) / dt);
+			ans.elex(0, 1, ans.ele(0, 1) / dt);
 			t.x = ans.ele(0, 0);
 			t.y = ans.ele(0, 1);
 			t.colour = ori.colour;
@@ -82,18 +63,51 @@ namespace FR3DCMD{
 			trans.elex(1, 0, 0);			trans.elex(1, 1, ca);			trans.elex(1, 2, -sa);			trans.elex(1, 3, 0);
 			trans.elex(2, 0, 0);			trans.elex(2, 1, sa);			trans.elex(2, 2, ca);			trans.elex(2, 3, 0);
 			trans.elex(3, 0, 0);			trans.elex(3, 1, 0);			trans.elex(3, 2, 0);			trans.elex(3, 3, 1);
-			o.elex(0, 0, ori.x);			o.elex(0, 1, ori.y);			o.elex(0, 2, ori.z);			o.elex(0, 3,1);
+			o.elex(0, 0, ori.x);			o.elex(0, 1, ori.y);			o.elex(0, 2, ori.z);			o.elex(0, 3, 1);
 
 			matrix ans = o * trans;
 			ori.x = ans.ele(0, 0);
 			ori.y = ans.ele(0, 1);
 			ori.z = ans.ele(0, 2);
 		}
+
+		void y_rotate(vertex &ori, double angle) {
+			matrix o(1, 4), trans(4, 4);
+			double  sa = sin(angle), ca = cos(angle);
+			trans.elex(0, 0, ca);			trans.elex(0, 1, 0);			trans.elex(0, 2, -sa);			trans.elex(0, 3, 0);
+			trans.elex(1, 0, 0);			trans.elex(1, 1, 1);			trans.elex(1, 2, 0);			trans.elex(1, 3, 0);
+			trans.elex(2, 0, sa);			trans.elex(2, 1, 0);			trans.elex(2, 2, ca);			trans.elex(2, 3, 0);
+			trans.elex(3, 0, 0);			trans.elex(3, 1, 0);			trans.elex(3, 2, 0);			trans.elex(3, 3, 0);
+			o.elex(0, 0, ori.x);			o.elex(0, 1, ori.y);			o.elex(0, 2, ori.z);			o.elex(0, 3, 1);
+
+			matrix ans = o * trans;
+			ori.x = ans.ele(0, 0);
+			ori.y = ans.ele(0, 1);
+			ori.z = ans.ele(0, 2);
+		}
+
+		void z_rotate(vertex &ori, double angle) {
+			matrix o(1, 4), trans(4, 4);
+			double  sa = sin(angle), ca = cos(angle);
+			trans.elex(0, 0, ca);			trans.elex(0, 1, sa);			trans.elex(0, 2, 0);			trans.elex(0, 3, 0);
+			trans.elex(1, 0, -sa);			trans.elex(1, 1, ca);			trans.elex(1, 2, 0);			trans.elex(1, 3, 0);
+			trans.elex(2, 0, 0);			trans.elex(2, 1, 0);			trans.elex(2, 2, 1);			trans.elex(2, 3, 0);
+			trans.elex(3, 0, 0);			trans.elex(3, 1, 0);			trans.elex(3, 2, 0);			trans.elex(3, 3, 1);
+			o.elex(0, 0, ori.x);			o.elex(0, 1, ori.y);			o.elex(0, 2, ori.z);			o.elex(0, 3, 1);
+
+			matrix ans = o * trans;
+			ori.x = ans.ele(0, 0);
+			ori.y = ans.ele(0, 1);
+			ori.z = ans.ele(0, 2);
+		}
+
 	private:
 		FR2DCMD::FR2D_CMD * F2DH;
-		double ox, oy,oz;
+		double ox, oy, oz;
 		double d;
 	};
+
+	//////end of FR3D_CMD class
 
 	class buffer {
 
